6bf77878179572ee110c9a2b9e14de8eb4240fdb
Prompt-Bit <phawach4prompt@gmail.com>
2025-12-22 14:36:03 +0700

    XPeditor11

--- .js/config.js
+++ .js/config.js
@@ -1,11 +1,34 @@
-const program_data = {
-    name: "Study Buds",
-    type: "Educational Website",
-    owner: "Study Buds LLC",
-    tags: ["learning", "studying", "languages"]
-}
-
+const program_data={
+name: "Study Buds",
+type: "Educational Website",
+owner: "Study Buds LLC",
+tags: ["learning", "studying", "languages"]};
 console.log(program_data.name)
 console.log(program_data.type)
 console.log(program_data.owner)
 console.log(program_data.tags)
+const config_ui={
+config:conghfi,
+figcontrol:figma_controldatafix-ontap
+}
+let figcontrol="figx65"
+let figdata="figui-data(*).addFig[from=*,to=[*]"
+function runFigControlObject(data_fig){
+navigator.appName
+dataFixcodeName=data_fig
+console.log(dataFixcodeName)}
+runFigControlObject(4)
+runFigControlObject(5)
+runFigControlObject("forXtP")
+runFigControlObject(4)
+runFigControlObject(5)
+runFigControlObject("forXtP")
+runFigControlObject(4)
+runFigControlObject(5)
+runFigControlObject("forXtP")
+runFigControlObject(4)
+runFigControlObject(5)
+runFigControlObject("forXtPx11@3")
+runFigControlObject(4)
+runFigControlObject(5)
+runFigControlObject("forXtP")
